b530a09d551874d899a2b06455c94cf3cad23959
vadym <vsukolen@anaqua.com>
2023-08-03 05:44:06 +0800

    fix action

--- download_parser.sh
+++ download_parser.sh
@@ -1,2 +1,2 @@
-wget -o coverage-notes https://github.com/sukolenvo/coverage-notes/releases/download/v$1/coverage-notes--$1-$(uname)-$(arch)
+wget -O coverage-notes https://github.com/sukolenvo/coverage-notes/releases/download/v$1/coverage-notes-$1-$(uname)-$(arch)
 chmod +x coverage-notes
